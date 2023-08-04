e940c10a0b642362d12227929a5715a263efc571
Olamilekan Lamidi <olamilekan.lamidi@vacancysoft.com>
2023-08-04 08:30:31 +0100

    remove e2e test

--- test/app.e2e-spec.ts
+++
@@ -1,5 +0,0 @@
-import { Test, TestingModule } from '@nestjs/testing';
-import { INestApplication } from '@nestjs/common';
-import * as request from 'supertest';
-import { AppModule } from './../src/app.module';
-import { BookModule } from './../src/book/book.module';
